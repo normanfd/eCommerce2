1f53aadfa8f24abca4f908f1b1e89ecc9e89dc77
normanfd <norman_fd@apps.ipb.ac.id>
2019-04-14 14:00:07 +0700

    add interface addnewproduct

--- app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminAddNewProductActivity.java
@@ -2,17 +2,30 @@ package com.example.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String CategoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
         CategoryName = getIntent().getExtras().get("category").toString();
         Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show();
+
+        AddNewProductButton = (Button) findViewById(R.id.add_new_product);
+        InputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        InputProductName = (EditText) findViewById(R.id.product_name);
+        InputProductDescription = (EditText) findViewById(R.id.product_description);
+        InputProductPrice = (EditText) findViewById(R.id.product_price);
+
     }
 
 }
